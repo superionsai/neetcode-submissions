8ee25148cee045de59c6f499b0ae653b2a7c666d
Penugonda Sai Sindhush <cs1240303@iitd.ac.in>
2026-06-08 23:43:22 +0200

    Add: simplify-path - submission-5

---
+++ Data Structures & Algorithms/simplify-path/submission-5.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack <string> comms;
+        string temp;
+        for (auto it: path) {
+            if (it == '/') {
+                if (temp != "") comms.push(temp);
+                temp = "";
+            }
+            else {
+                temp += it;
+            }
+        }
+        if (temp != "") comms.push(temp);
+
+        string parsedPath = ""; int popCount = 0;
+        while (!comms.empty()) {
+            temp = comms.top();
+            cout << temp << " " << popCount << endl;
+            if (temp == "..") {
+                popCount++;
+                comms.pop();
+            }
+            else if (temp == ".") { comms.pop(); }
+            else {
+                if (popCount) { 
+                    popCount--; 
+                    comms.pop(); 
+                }
+                else {
+                    parsedPath = ('/' + temp + parsedPath);
+                    comms.pop();
+                }
+            }
+        }
+        
+        return (parsedPath=="") ? "/" : parsedPath;
+    }
+};
