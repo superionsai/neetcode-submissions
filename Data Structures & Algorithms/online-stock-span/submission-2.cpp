79d483e25c758e419ab9893156a7c091ad413198
Penugonda Sai Sindhush <cs1240303@iitd.ac.in>
2026-06-08 16:17:13 +0200

    Add: online-stock-span - submission-2

---
+++ Data Structures & Algorithms/online-stock-span/submission-2.cpp
@@ -0,0 +1,26 @@
+class StockSpanner {
+private:
+    vector <pair<int, int>> Spanner;
+
+public:
+    StockSpanner() { 
+        Spanner.push_back({INT_MAX, 0});
+    }
+    
+    int next(int price) {
+        int iter = Spanner[0].second; 
+        int counter = 1;
+        int span = 1;
+        while (iter > 0 && Spanner[iter].first <= price) { counter++; iter--; }
+        span = max(span, counter);
+        Spanner.push_back({price, span});
+        Spanner[0].second++;
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
