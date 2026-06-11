95f604a91b290748d0a6f48ffd1069f1b9ab3343
Penugonda Sai Sindhush <cs1240303@iitd.ac.in>
2026-06-11 16:53:04 +0200

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+private:
+    unordered_map <TreeNode*, int> depthMap;
+    int _maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        if (depthMap.find(root->left) == depthMap.end())
+            depthMap[root->left] = _maxDepth(root->left);
+        if (depthMap.find(root->right) == depthMap.end())
+            depthMap[root->right] = _maxDepth(root->right);
+
+        depthMap[root] = max(1 + depthMap[root->left], 1 + depthMap[root->right]);
+        return depthMap[root];
+    }
+
+public:
+    bool isBalanced(TreeNode* root) {
+        if (root == nullptr) return true;
+        int maxDepth = _maxDepth(root);
+
+        if (abs(depthMap[root->left] - depthMap[root->right]) <= 1)
+            return (isBalanced(root->left) && isBalanced(root->right));    
+        return false;    
+    }
+};
