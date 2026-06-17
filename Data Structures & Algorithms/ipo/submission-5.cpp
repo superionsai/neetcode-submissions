0b4ee6a5b4b833d9f089a134903ebbe48dbbeaef
Penugonda Sai Sindhush <cs1240303@iitd.ac.in>
2026-06-17 13:56:23 +0200

    Add: ipo - submission-5

---
+++ Data Structures & Algorithms/ipo/submission-5.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<vector<int>> projects; int n = capital.size();
+        for (int i = 0; i < n; i++) {
+            projects.push_back({capital[i], profits[i]});
+        }
+        sort(projects.begin(), projects.end());
+
+        int cap = w; int i = 0; int count = 0;
+        struct Cmp {
+            const vector<vector<int>>* p;
+            bool operator()(int a, int b) const {
+                return (*p)[a][1] < (*p)[b][1];
+            }
+        };
+        Cmp cmp1{&projects};
+        priority_queue <int, vector<int>, decltype(cmp1)> heap(cmp1);
+        while (i < n || !heap.empty()) {
+            while (i < n && projects[i][0] <= cap) {
+                heap.push(i); i++;
+            }
+            if (heap.empty()) break;
+            int curr = heap.top();
+            if (projects[curr][1] > 0) {
+                cap += projects[curr][1];
+                count++;
+            }
+            if (count == k) break;
+            heap.pop();
+        }
+
+        return cap;
+    }
+};
