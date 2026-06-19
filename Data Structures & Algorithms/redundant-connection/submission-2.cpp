de33eead27d9864b5c99f1ed97afa98ef3c55658
Penugonda Sai Sindhush <cs1240303@iitd.ac.in>
2026-06-19 10:26:12 +0200

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.cpp
@@ -0,0 +1,51 @@
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 1);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int node) {
+        int cur = node;
+        while (cur != parent[cur]) {
+            parent[cur] = find(parent[cur]);
+            cur = parent[cur];
+        }
+        return cur;
+    }
+
+    bool unionSets(int u, int v) {
+        int pu = find(u);
+        int pv = find(v);
+        if (pu == pv) {
+            return false;
+        }
+        if (rank[pv] > rank[pu]) {
+            swap(pu, pv);
+        }
+        parent[pv] = pu;
+        rank[pu] += rank[pv];
+        return true;
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        DSU dsu(n+1);
+        vector<int> ans;
+        for (auto& edge : edges) {
+            if (!dsu.unionSets(edge[0], edge[1])) {
+                return edge;
+            }
+        }
+        return ans;
+    }
+};
