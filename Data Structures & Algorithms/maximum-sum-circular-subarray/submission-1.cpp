03d698b24a29c553bae49f7ba1d2e948fd63e6c9
Penugonda Sai Sindhush <cs1240303@iitd.ac.in>
2026-06-20 21:10:16 +0200

    Update: maximum-sum-circular-subarray - submission-1

--- Data Structures & Algorithms/maximum-sum-circular-subarray/submission-1.cpp
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-1.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int currSum = nums[0], maxSum = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            currSum = max(nums[i], currSum + nums[i]);
-            maxSum = max(currSum, maxSum);
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int globMax = nums[0], globMin = nums[0];
+        int curMax = 0, curMin = 0, total = 0;
+
+        for (int& num : nums) {
+            curMax = max(curMax + num, num);
+            curMin = min(curMin + num, num);
+            total += num;
+            globMax = max(globMax, curMax);
+            globMin = min(globMin, curMin);
         }
-        return maxSum;
+
+        return globMax > 0 ? max(globMax, total - globMin) : globMax;
     }
 };
