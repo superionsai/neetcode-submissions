eaa90cc64164fb7dbea60cb45b4980c62e84dbe8
Penugonda Sai Sindhush <cs1240303@iitd.ac.in>
2026-06-21 22:09:36 +0200

    Add: jump-game - submission-5

---
+++ Data Structures & Algorithms/jump-game/submission-5.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int currForward = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > currForward) return false;
+            currForward = max(currForward, nums[i] + i);
+        }
+        return true;
+    }
+};
