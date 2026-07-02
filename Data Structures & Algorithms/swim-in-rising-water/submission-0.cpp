02bde33188ed3ee300f5753e362026fa04f8cee0
Penugonda Sai Sindhush <cs1240303@iitd.ac.in>
2026-07-02 02:40:58 +0200

    Add: swim-in-rising-water - submission-0

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-0.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+
+        priority_queue<
+            vector<int>,
+            vector<vector<int>>,
+            greater<vector<int>>
+        > pq;
+
+        dist[0][0] = grid[0][0];
+        pq.push({grid[0][0], 0, 0});
+
+        while (!pq.empty()) {
+            auto curr = pq.top();
+            pq.pop();
+
+            int cost = curr[0];
+            int i = curr[1];
+            int j = curr[2];
+
+            if (i == n - 1 && j == n - 1)
+                return cost;
+
+            if (cost > dist[i][j])
+                continue;
+
+            if (i > 0) {
+                int newCost = max(cost, grid[i - 1][j]);
+                if (newCost < dist[i - 1][j]) {
+                    dist[i - 1][j] = newCost;
+                    pq.push({newCost, i - 1, j});
+                }
+            }
+
+            if (i < n - 1) {
+                int newCost = max(cost, grid[i + 1][j]);
+                if (newCost < dist[i + 1][j]) {
+                    dist[i + 1][j] = newCost;
+                    pq.push({newCost, i + 1, j});
+                }
+            }
+
+            if (j > 0) {
+                int newCost = max(cost, grid[i][j - 1]);
+                if (newCost < dist[i][j - 1]) {
+                    dist[i][j - 1] = newCost;
+                    pq.push({newCost, i, j - 1});
+                }
+            }
+
+            if (j < n - 1) {
+                int newCost = max(cost, grid[i][j + 1]);
+                if (newCost < dist[i][j + 1]) {
+                    dist[i][j + 1] = newCost;
+                    pq.push({newCost, i, j + 1});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
